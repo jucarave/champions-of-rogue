5f6215939a6b1aa52db82be7191c824c350861b9
Camilo Ramirez <jucarave92@hotmail.com>
2016-10-21 18:40:51 -0500

    COR-4 Added documentation to Utils object.

--- src/Utils.ts
+++ src/Utils.ts
@@ -5,20 +5,37 @@ import { Tile } from './engine/Tile';
 import { Character } from './engine/Character';
 
 let Utils = {
-    rollDice(value: string): number {
-        let array: Array<string> = value.split(/[D\+*]/),
-            a: number = parseInt(array[0], 10),
-            b: number = parseInt(array[1], 10),
-            c: number = parseInt(array[2], 10) || 0;
+    /**
+     * Rolls a 'y' sided dice 'x' times and sum the results
+     * to z
+     * 
+     * @param dice String: Dice with the format "xDy+z" 
+     * 
+     * @return Number: Result of the rolled 
+     */
+    rollDice(dice: string): number {
+        let array: Array<string> = dice.split(/[D\+*]/),
+            x: number = parseInt(array[0], 10),
+            y: number = parseInt(array[1], 10),
+            z: number = parseInt(array[2], 10) || 0;
 
-        let ret: number = c;
-        for (let i = 0; i < a; i++) {
-            ret += (Math.round(Math.random() * (b - 1))) + 1;
+        let ret: number = z;
+        for (let i = 0; i < x; i++) {
+            ret += (Math.round(Math.random() * (y - 1))) + 1;
         }
 
         return ret;
     },
 
+    /**
+     * Separates a single string line into multiple lines
+     * defined by a monospaced width. 
+     *  
+     * @param text String: Full string that has to be splitted.
+     * @param width Number: Maximum width per line.
+     * 
+     * @return Array<String>: Array containing the string separated.
+     */
     formatText(text: string, width: number): Array<string> {
         let ret: Array<string> = [],
             words: Array<string> = text.split(" "),
@@ -43,6 +60,18 @@ let Utils = {
         return ret;
     },
 
+    /**
+     * Transform a invalid variable code name like "!" into 
+     * its valid form ("EXCLA") and returns the tile using 
+     * the correct name.
+     * 
+     * @param Renderer: Renderer instance to obtain the final tile. 
+     * @param chara String: Character to represent the tile.
+     * @param Color: Foreground color of the tile.
+     * @param backColor?: Background color of the tile (Default BLACK).
+     * 
+     * @return Tile: Tile served by the rendering class.
+     */
     getTile: function (renderer: Renderer, chara: string, color: Color, backColor: Color = Colors.BLACK): Tile {
         var tile = chara;
 
@@ -69,6 +98,16 @@ let Utils = {
         return renderer.getTile(backColor, color, <Character>Tiles[tile]);
     },
 
+    /**
+     * Plots a string line into the canvas.
+     * 
+     * @param Renderer: Renderer instance to plot the characters.
+     * @param x Number: X position of the text.
+     * @param y Number: Y position of the text.
+     * @param text String: Text to render on the canvas.
+     * @param Color?: Foreground color of the text (Default WHITE).
+     * @param backColor?: BackgroundColor of the text (Default BLACK).
+     */
     renderText(renderer: Renderer, x: number, y: number, text: string, color: Color = Colors.WHITE, backColor: Color = Colors.BLACK) {
         for (let i = 0; i < text.length; i++) {
             let t: string = text[i];
@@ -81,6 +120,14 @@ let Utils = {
         }
     },
 
+    /**
+     * Makes a GET Http request call and tries to obtain a
+     * JSON object from the response.
+     * 
+     * @param url String: Absolute url to perform the call
+     * @param callback Function(jsonObject): Callback function 
+     *         to return the data 
+     */
     loadJSON(url: string, callback: Function) {
         let http: XMLHttpRequest = new XMLHttpRequest();
         http.open("GET", url, true);
